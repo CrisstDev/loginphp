6a62e8e04966b3e33fb63d17dfff97ac9b5628fd
CrisstDev <cctian7@gmail.com>
2022-06-07 21:56:47 -0500

    Initial commit

---
+++ pag_user.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="vaidroll.css">	
+</head>
+<body>
+<table>
+<th>Bienvenido usuario</th><th><a href="index.html">Regresar</a></th>
+<a href="index.html">Regresar</a>
+</table>
+</body>
+</html>
